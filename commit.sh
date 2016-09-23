38cae03efc50b088292efab0959cc014515c04e2
弈辰 <huijian.xhj@alibaba-inc.com>
2016-09-23 20:18:17 +0800

    转载我 CSDN 的一篇博文

--- commit.sh
+++ commit.sh
@@ -1,1 +1,11 @@
-git add . && git commit -m "update" && git push origin master
+#!/bin/bash
+# desc: git add and commit to local storage
+
+# commit message
+comment=$*
+
+if [ ! -n "$comment" ]; then
+   git add . && git commit -m "update"
+else
+   git add . && git commit -m "$comment"
+fi
